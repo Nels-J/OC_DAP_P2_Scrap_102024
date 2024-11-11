c64030a01093e3b492126c2c07ab62f8a9f44445
Nelson Jesus <n.jesus@tracknco.com>
2024-11-11 22:28:09 +0100

    Fix: Only use the numerical quantity of availability in get_book_infos()

--- booktoscrap/book.py
+++ booktoscrap/book.py
@@ -1,8 +1,8 @@
+import re
 from urllib.parse import urljoin
 
 from booktoscrap.category import BASE_CATALOGUE_URL
 from booktoscrap.connection import get_soup
-
 
 
 def get_book_infos(book_url: str) -> dict[str, str]:
@@ -25,17 +25,11 @@ def get_book_infos(book_url: str) -> dict[str, str]:
     if star_rating:
         # On liste les classes présentes dans le tag,
         # puis on extrait la classe additionnelle différente de 'star-rating'
-        class_list = star_rating.get('class')
-        additional_class = [cls for cls in class_list if cls != 'star-rating'][0]
+        class_list = star_rating.get("class")
+        additional_class = [cls for cls in class_list if cls != "star-rating"][0]
         # Transformation de la classe en 'nombre' pour faciliter l'exploitation.
-        rating_dict = {
-            'One': '1',
-            'Two': '2',
-            'Three': '3',
-            'Four': '4',
-            'Five': '5'
-        }
-        rating_value = rating_dict.get(additional_class, 0)  # Retourne 0 si la classe n'est pas dans le dictionnaire.
+        rating_dict = {"One": "1", "Two": "2", "Three": "3", "Four": "4", "Five": "5"}
+        rating_value = rating_dict.get(additional_class, 0)  # Si non défini 0.
         # On ajoute cette note à notre dictionnaire
         book_informations["Rating"] = rating_value
 
@@ -44,13 +38,24 @@ def get_book_infos(book_url: str) -> dict[str, str]:
     for row in table.find_all("tr"):
         key = row.find("th").text.strip()
         value = row.find("td").text.strip()
+
+        # On nettoie l'interprétation monétaire.
         if "Price" or "Tax" in key:
             value = value.replace("Â£", "£").strip()
             # On ajoute les infos au dictionnaire.
             book_informations[key] = value
+
+        # On extrait que la quantité en stock et non le message.
+        if "Availability" in key:
+            match = re.search(r"\((\d+)\s+available\)", value)
+            if match:
+                book_informations[key] = match.group(1)
+
     return book_informations
 
+
 # TODO get img_url
+
 
 def generate_all_book_urls_from_category_page(category_page_url: str) -> list:
     """
@@ -74,9 +79,7 @@ def generate_all_book_urls_from_category_page(category_page_url: str) -> list:
 
 
 if __name__ == "__main__":
-    print(
-        get_book_infos(f"{BASE_CATALOGUE_URL}in-a-dark-dark-wood_963/index.html")
-    )
+    print(get_book_infos(f"{BASE_CATALOGUE_URL}in-a-dark-dark-wood_963/index.html"))
     for book in generate_all_book_urls_from_category_page(
         f"{BASE_CATALOGUE_URL}category/books/mystery_3/index.html"
     ):
