384990e53f4a6f10be1c14dee85099ec10e91261
Nelson Jesus <n.jesus@tracknco.com>
2024-12-01 23:32:09 +0100

    Add book category name + renaming dict keys, fix an OR conditional statement, fix datatypes, refacto a regex for clarity

--- booktoscrap/book.py
+++ booktoscrap/book.py
@@ -17,8 +17,17 @@ def get_book_infos(book_url: str) -> dict[str, str]:
     """
     soup = get_soup(book_url)
 
-    # On récupère le titre du livre qu'on ajoute à notre dictionnaire.
-    book_informations = {"Title": soup.find("h1").text.strip()}
+    # On récupère le titre du livre son URL qu'on ajoute à notre dictionnaire.
+    book_informations = {"title": soup.find("h1").text.strip(), "product_page_url": book_url}
+
+    # On récupère la catégorie du livre qu'on ajoute à notre dictionnaire.
+    book_category = soup.find('ul', class_='breadcrumb')
+    breadcrumb_links = book_category.find_all('a')
+    if breadcrumb_links:
+        last_breadcrumb_link = breadcrumb_links[-1].get_text()
+        book_informations["category"] = last_breadcrumb_link
+    else:
+        book_informations["category"] = "Undefined category"
 
     # On récupère la classe représentant la note des lecteurs.
     star_rating = soup.find("p", class_="star-rating")
@@ -27,35 +36,53 @@ def get_book_infos(book_url: str) -> dict[str, str]:
         # puis on extrait la classe additionnelle différente de 'star-rating'
         class_list = star_rating.get("class")
         additional_class = [cls for cls in class_list if cls != "star-rating"][0]
-        # Transformation de la classe en 'nombre' pour faciliter l'exploitation.
-        rating_dict = {"One": "1", "Two": "2", "Three": "3", "Four": "4", "Five": "5"}
+        # Transformation de la classe en 'nombre entier' pour faciliter l'exploitation.
+        rating_dict = {"One": 1, "Two": 2, "Three": 3, "Four": 4, "Five": 5}
         rating_value = rating_dict.get(additional_class, 0)  # Si non défini 0.
         # On ajoute cette note à notre dictionnaire
-        book_informations["Rating"] = rating_value
+        book_informations["review_rating"] = rating_value
 
     # On récupère le lien relatif de l'image du livre, qu'on transforme en absolu.
     active_item = soup.find("div", class_="item active")
     base_url = BASE_CATALOGUE_URL[:-10]
     img_url = urljoin(base_url, active_item.find("img").attrs["src"])
-    book_informations["Picture"] = img_url
+    book_informations["image_url"] = img_url
 
-    # On récupère les informations du livre présentes dans un tableau.
+    # On récupère les informations du livre présentes dans le tableau.
     table = soup.find("table", class_="table table-striped")
+
+    book_table_headers = {
+        'UPC': 'universal_product_code',
+        'Price (incl. tax)': 'price_including_tax',
+        'Price (excl. tax)': 'price_excluding_tax',
+        'Availability': 'number_available',
+    }
+
     for row in table.find_all("tr"):
         key = row.find("th").text.strip()
         value = row.find("td").text.strip()
+        if key not in book_table_headers:
+            continue
 
         # On nettoie l'interprétation monétaire.
-        if "Price" or "Tax" in key:
+        if "Price" in key or "Tax" in key:
             value = value.replace("Â£", "£").strip()
-            # On ajoute les infos au dictionnaire.
-            book_informations[key] = value
 
-        # On extrait que la quantité en stock et non le message.
-        if "Availability" in key:
-            match = re.search(r"\((\d+)\s+available\)", value)
+        # On extrait la quantité dans un texte 'In stock (18 available)'
+        elif key == "Availability":
+            regex_pattern = r"""
+                \(              # on récupère à partir de la parenthèse ouvrante
+                (\d+)           # un ou plusieurs Digit qui seront
+                \s+             # suivis d'un ou plusieurs eSpaces blancs
+                available       # et du mot available
+                \)              # on arrête la regex dès la parenthèse fermante
+            """
+            # L'emploi de verbose m'a permis de détailler la regex ci-dessus
+            match = re.search(regex_pattern, value, re.VERBOSE)
             if match:
-                book_informations[key] = match.group(1)
+                value = int(match.group(1)) # groupe 0 est égal à la string entière
+
+        book_informations[book_table_headers[key]] = value
 
     return book_informations
 
