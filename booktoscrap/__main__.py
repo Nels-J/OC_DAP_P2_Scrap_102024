0888d3fe85f13948569b21bd50dea7bcbc9d9e06
Nelson Jesus <n.jesus@tracknco.com>
2024-12-02 12:19:21 +0100

    Added category_name parameter on scrapbooks signature

--- booktoscrap/__main__.py
+++ booktoscrap/__main__.py
@@ -9,7 +9,7 @@ from booktoscrap.category import (
 from booktoscrap.csv_generator import create_books_informations_csv_file
 
 
-def scrapbooks(category_url):
+def scrapbooks(category_url, category_name):
 
     # Génère la liste des pages liées à chacune des catégories listées (pagination)
     for category_page in list_product_page_links_from_category(category_url):
@@ -17,6 +17,7 @@ def scrapbooks(category_url):
         for book_url in generate_all_book_urls_from_category_page(category_page):
             # Pour chaque livre récupère la grille d'informations
             book_info = get_book_infos(book_url)
+            book_info["category_name"] = category_name
             yield book_info
 
 
@@ -24,5 +25,5 @@ if __name__ == "__main__":
     for category_name, category_url in get_categories_from_home(
         "https://books.toscrape.com/"
     ):
-        category_books = scrapbooks(category_url)
+        category_books = scrapbooks(category_url, category_name)
         create_books_informations_csv_file(f"{category_name}.csv", category_books)
