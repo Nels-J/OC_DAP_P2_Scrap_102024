bae089decb496527cbdbbc729fddf08e60288cbe
Nelson Jesus <n.jesus@tracknco.com>
2024-12-10 11:46:15 +0100

    flake8 fixes on img_downloader.py

--- booktoscrap/img_downloader.py
+++ booktoscrap/img_downloader.py
@@ -3,14 +3,19 @@ import os
 import requests
 
 
-def download_image(category_dir:str, category_name:str, image_url: str, book_upc: str):
-        response = requests.get(image_url)
+def download_image(
+        category_dir: str,
+        category_name: str,
+        image_url: str,
+        book_upc: str
+):
+    response = requests.get(image_url)
 
-        # Isoler l'extension image dans l'url, puis reconstituer le nom
-        file_extension = os.path.splitext(image_url)[-1].lower()
-        filename = f"{category_dir}/{category_name}_upc_{book_upc}{file_extension}"
+    # Isoler l'extension image dans l'url, puis reconstituer le nom
+    file_extension = os.path.splitext(image_url)[-1].lower()
+    filename = f"{category_dir}/{category_name}_upc_{book_upc}{file_extension}"
 
-        with open(filename, 'wb') as file:
-            file.write(response.content)
+    with open(filename, 'wb') as file:
+        file.write(response.content)
 
-        print(f"Fichier image enregistrée sous: {filename}")
+    print(f"Fichier image enregistrée sous: {filename}")
